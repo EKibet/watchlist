a89dd9e6e464baf97240e4c56bc735eb48f12c1f
EKibet <kibetedgar@gmail.com>
2018-09-06 16:05:00 +0300

    Recommit

---
+++ instance/config.py
@@ -0,0 +1,2 @@
+MOVIE_API_KEY='3ce26cd387a70e2835c5f6005d82bdc5'
+SECRET_KEY='nanciekathini'
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY=3ce26cd387a70e2835c5f6005d82bdc5
+export SECRET_KEY='Flask WTF Secret Key'
+export MAIL_USERNAME=kibetedgar@gmail.com
+export MAIL_PASSWORD=31834310
+python3.6 manage.py server
